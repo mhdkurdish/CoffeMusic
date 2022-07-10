17cce7e996743f2a1ba9140199d47adc926cb3fa
mhdkurdish <49838432+mhdkurdish@users.noreply.github.com>
2022-07-10 05:20:31 +0300

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,15 +57,15 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamYukki")
-                await self.one.join_chat("TheYukki")
-                await self.one.join_chat("YukkiSupport")
+                await self.one.join_chat("CoffeBotAssistant")
+                await self.one.join_chat("TheCoffe")
+                await self.one.join_chat("CoffeSupport")
             except:
                 pass
             assistants.append(1)
             try:
                 await self.one.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "یاریدەدەری بۆت دەست بەکار بوو"
                 )
             except:
                 LOGGER(__name__).error(
